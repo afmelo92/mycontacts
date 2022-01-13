e7bc79028fedeb3a7abb7e28b05e1db088a39a14
Andre <andre.fabian.melo@gmail.com>
2022-01-13 19:53:10 -0300

    Add contacts list cards widh data fetch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+// import Loader from '../../components/Loader';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import formatPhone from '../../utils/formatPhone';
 
 import * as S from './styles';
 
 function HomePage() {
+  const [contacts, setContacts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // setLoading(true);
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const data = await response.json();
+        setContacts(data);
+        // setLoading((prevState) => !prevState);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <S.Container>
       <S.InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />
       </S.InputSearchContainer>
       <S.Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </S.Header>
 
@@ -26,25 +47,27 @@ function HomePage() {
           </button>
         </header>
 
-        <S.Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Andre Melo</strong>
-              <small>instagram</small>
+        {contacts.map((contact) => (
+          <S.Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && <small>{contact.category_name}</small>}
+              </div>
+              <span>{contact.email}</span>
+              <span>{formatPhone(contact.phone)}</span>
             </div>
-            <span>andre@afmelo.com</span>
-            <span>(41) 9 9901-3657</span>
-          </div>
 
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </S.Card>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </div>
+          </S.Card>
+        ))}
 
       </S.ListContainer>
     </S.Container>
