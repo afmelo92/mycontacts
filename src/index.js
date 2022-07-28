41163e66e97688a945e2ca094efb60274ec16d1a
Andre Melo <andre.fabian.melo@gmail.com>
2022-07-27 22:38:37 -0300

    fix: old toast provider removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import { App } from 'components';
-import { ToastProvider } from 'hooks/useToast';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
